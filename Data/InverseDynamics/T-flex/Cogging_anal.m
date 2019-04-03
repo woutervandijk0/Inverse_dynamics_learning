f68acb3d333c9d6d99c3268e0305730939bf68b9
Wouter van Dijk <woutervandijk0@gmail.com>
2019-04-03 13:21:34 +0200

    add datasets

--- Data/InverseDynamics/T-flex/Cogging_anal.m
+++ Data/InverseDynamics/T-flex/Cogging_anal.m
@@ -1,13 +1,14 @@
 %% Cogging data analysis
 %enable or disable plot 1= true
-plots =1;
+plots =0;
 %close all
 % load data no mass
 matlab_data = SimulinkRealTime.utils.getFileScopeData('cogWMPCFR.DAT');
-matlab_data.data = matlab_data.data(9000:end-1000,:);
+matlab_data.data = matlab_data.data(20000:end-1000,:);
 
-% Import data
 tlong = matlab_data.data(:,end);
+% Import data
+
 %tlong = tlong(find((tlong<33)));
 %indi = find(tlong>20);
 %tlong = tlong(indi);
@@ -20,6 +21,20 @@ a = matlab_data.data(indi,6);
 isp = matlab_data.data(indi,2);
 ia = matlab_data.data(indi,3);
 m = matlab_data.data(indi,8);
+%}
+%{
+indi = 1:length(tlong);
+u = matlab_data.data(indi,3);
+y = matlab_data.data(indi,1);
+ysp = matlab_data.data(indi,6);
+v = matlab_data.data(indi,5);
+a = matlab_data.data(indi,6);
+isp = matlab_data.data(indi,2);
+n = matlab_data.data(indi,5);
+ia = matlab_data.data(indi,3);
+%m = matlab_data.data(indi,8);
+
+%}
 
 %show dirty imported data
 if plots ==1
@@ -63,6 +78,21 @@ if plots ==1
     plot(y,y-ysp)
 end
 
+figure
+vsp = diff(ysp);
+vsp(2368:35334:end) = -3e-3;
+vsp(21035:35334:end) = -3e-3;
+vsp(20035:35334:end) = 3e-3;
+vsp = [vsp(1);vsp];
+plot(vsp)
+
+figure
+asp = diff(vsp);
+asp = [asp(1);asp];
+plot(asp)
+
+
+%{
 %split data into movements
 [NispmP, NispmN]= splitdata(isp,m);
 [NiamP, NiamN]  = splitdata(ia,m);
@@ -71,7 +101,6 @@ end
 [NvmP,NvmN]     = splitdata(v,m);
 [NamP,NamN]     = splitdata(a,m);
 t = [0:0.001:(length(find(m==1))-1)/1000]';
-
 
 %% load data with mass
 matlab_data = SimulinkRealTime.utils.getFileScopeData('H2cog25V3WM.DAT');
@@ -152,3 +181,4 @@ plot(NymP,NiamP,WymP,WiamP)
 plot(mean(NymP,2),mean(NiamP,2),mean(WymP,2),mean(WiamP,2))
 
 end
+%}
