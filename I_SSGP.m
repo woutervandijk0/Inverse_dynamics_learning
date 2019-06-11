5415debb3b34e5324622d4722cc455510bfe0cc5
Wouter van Dijk <woutervandijk0@gmail.com>
2019-06-11 20:22:55 +0200

    Comparison between algorithms
    SSGP, I-SSGP, I-FITC

--- I_SSGP.m
+++ I_SSGP.m
@@ -1,430 +1,149 @@
-close all, clear all
+%% Data selection
+i_f = [1:500];
+i_s = [1:10:500];
+i_loop  = [600:700];
+i_plot  = [1:1000];
 
-%% Select and open Dataset ('Default' opens file explorer)
-dataID = 'TFlex1D-2.mat';
-%dataID = 'TFlex1D5G.mat';
-dataID = 'TFlexADRC_RN20.mat';
-%dataID = 'DoublePendulumRand.mat'
-%dataID  = 'DoublePendulumRand1.mat'
+f  = xTrain(:,i_f)';
+s  = xTrain(:,i_s)';
+loop = xTrain(:,i_loop)';
 
-%% Settings 
-param_update   =  true;  % Update hyperparameters in the loop (true/false)
-loadSetpoint   =  true;   % 
-compareWTrue   =  false;  % Compare with real data (only for periodic datasets)
-loadParams     =  true;   % Load hyper parameters from previous run
-saveParams     =  false;   %
-manualParams   =  false;  % Manualy set hyperparams
-fromScratch    =  true;  % Start with clean sheet
-onlyPredict    =  1e9;   % Only predict after the nth iteration
+yf = yTrain(:,i_f)';
+ys = yTrain(:,i_s)';
+yloop = yTrain(:,i_loop)';
 
-D = 50;    % Number of random features
-Z = 2000;    % Size of floating frame
-Z_resamp   = 100;
-maxIter    = 5e5;   % Max number of iterations
-trainIter  = 1;     % Number of Posterior updates per new sample
-trainSteps = 100;
-numDescent = 20;     % Number of fmincon iterations
-resamp = 1;
+[dof, N] = size(xTrain);
 
-%Learning Rates:
-alpha = 1;      % Update posterior
-kappa = 1;      % Prediction
-beta  = 1;      % Hyperparameters
-beta_min   = 0.001;
-beta_reduc = 0.9999
+%% I-SSGP
+fprintf('  ----  Streaming sparse GP - Bui2015  ----   \n')
+ % Number of random features
+D = 50;   
+sn2 = sn.^2;
 
-%% Load dataset & Normalize 
-% Load
-[X,Y,ts,N_io] = selectData('dataset',dataID,'fig',true,'figNum',1);
-
-n = size(X,2);      % Number of inputs
-Q = size(Y,2);      % Number of latent functions
-
-% Normalize by limits of system;
-mu_X  = zeros(1,size(X,2));%mean(X);
-sig_X = ones(1,size(X,2)).*0.5% std(X);
-X = (X - mu_X) ./ sig_X;
-
-%Load setpoint
-if loadSetpoint == true
-    Xsp = loadSp(dataID);
-    Xsp = (Xsp - mu_X) ./ sig_X;
-else
-    Xsp = X;
-end
-
-% Normalize
-mu_Y  = zeros(1,size(Y,2));%mean(Y);
-sig_Y = [50 15];%std(Y);
-mu_Y  = mean(Y);
-sig_Y = std(Y);
-Y = (Y - mu_Y) ./ sig_Y;
-%Y = movmean(Y,30);
-
-%
-t_learn = 50;
-X = X(t_learn/ts:end,:);
-Y = Y(t_learn/ts:end,:);
-Xsp = Xsp(t_learn/ts:end,:);
-%}
-%% Hyperparam optimization settings
-%options = optimset();
-options.Display = 'none';
-options.MaxIter = numDescent;
-%options.Hessian = 'off';
-%options.Method = 'sqp';
-%active-set | interior-point | interior-point-convex | levenberg-marquardt | ...
-                           %sqp | trust-region-dogleg | trust-region-reflective
-%options.TolFun  = 1e-3;
-%options.UseParallel = true
-
-%% Hyperparameters (default unit)
-% Likelihood
-sn      =   ones(Q,1)*0.0603;
-% Covariance
-sf      =   ones(Q,1)*1;        % Std covariance (at t=0, 1)
-ell     =   ones(Q,n)*1;       % Length scales (at t=0, 1)
-
-if loadParams == true
-        [~, sf, ell] = loadHyperparams(dataID,false);
-%        ell = ell'; sf = sf'; sn = sn'; %Use transpose
-        disp([char(10),'Hyperparams loaded for: ',dataID]) 
-        disp(['ell: ',num2str(ell(1,:))])
-        disp(['sf : ',num2str(sf(1:Q)')]);
-        disp(['sn : ',num2str(sn(1:Q)'),char(10)]);
-end
-
-%Combine to 1 vector;
-disp('Final hyperparameters: ')
-disp(['ell: ',num2str(ell(1,:))])
-disp(['sf : ',num2str(sf(1:Q)')]);
-disp(['sn : ',num2str(sn(1:Q)'),char(10)]);
-
-hyp     =   abs([ell(:,1:n),sf]);
-
-%% Run incremental sparse spectrum GP
 % Preallocate vectors and matrices
-if fromScratch == true
-    numIter  = min(length(X),maxIter);
-    muPred   = zeros(numIter,Q);
-    yhat     = zeros(numIter,Q);
-    yhatSp   = zeros(numIter,Q);
-    s2       = zeros(numIter,Q);
-    s2Sp     = zeros(numIter,Q);
-    nlml     = zeros(numIter,Q);
-    HYP      = zeros(Q,numIter,n+1);
-    phi_x    = zeros(Z,Z);
-    
-    w    = zeros(2*D,Q);
-    b    = zeros(2*D,Q);
-    v    = zeros(2*D,Q);
-    phi  = zeros(2*D,Q);
-    
-    RAND  = randn(D,n);
-    R = zeros(Q,2*D,2*D);
-    for i = 1:Q
-        R(i,:,:) = eye(2*D,2*D)*sn(Q);
-        SIGMA(i,1:D,1:n) = RAND.*hyp(i,1:n);
-    end
-    sqrtD =  sqrt(D);
-    sn2 = sn.^2;
-end
-iter = Z;
-iterHypOpti = 0;
+mu_s   = zeros(length(i_s),1);
+var_s  = zeros(length(i_s),1);
 
-%Start loop
-tic;
-for ii = Z:numIter
-    %Prediction with setpoint
-    for i = 1:Q
-        phi      = sf(i)./sqrtD .*[cos(squeeze(SIGMA(i,:,:))*Xsp(ii,1:n)')',...
-            sin(squeeze(SIGMA(i,:,:))*Xsp(ii,1:n)')']';
-        yhatSp(iter,i) = (1-kappa).*yhatSp(iter-1,i) + kappa*dot(w(:,i),phi);
-        v(:,i)        = squeeze(R(i,:,:))'\phi;
-        s2Sp(iter,i)   = sn2(i).*(1+dot(v(:,i),v(:,i)));
-        
-        % When setpoint and measured data is different
-        if loadSetpoint == true
-            phi      = sf(i)./sqrtD .*[cos(squeeze(SIGMA(i,:,:))*X(ii,1:n)')',...
-                sin(squeeze(SIGMA(i,:,:))*X(ii,1:n)')']';
-        end
-        yhat(iter,i) = (1-kappa).*yhat(iter-1,i) + kappa*dot(w(:,i),phi);
-        v(:,i)        = squeeze(R(i,:,:))'\phi;
-        s2(iter)   = sn2(i).*(1+dot(v(:,i),v(:,i)));
-        
-        % Hyperparameter optimization
-        if param_update == true && iter >= 1 && mod(iter,trainSteps) == 0
-            y    = Y((ii-Z+1):Z_resamp:ii,i);
-            x    = X((ii-Z+1):Z_resamp:ii,1:n);
-            hyp_opt = hyp(i,:);
-            [hyp(i,:) nlml(iter,i)] = fminsearch(@(hyp_opt) calcNLML(hyp_opt,sn2(i),x,y,Z,D,RAND,n),hyp_opt,options);
-            hyp(i,:) = (1-beta).*hyp_opt + beta.*hyp(i,:);
-            HYP(i,iter,:) = hyp(i,1:n+1);     %Store current hyperparams
-            sf(i)    = hyp(i,end);
-            SIGMA(i,1:D,1:n) = RAND.*hyp(i,1:n);
-            iterHypOpti = iterHypOpti+1;
-        end
-        if iter < onlyPredict
-            %Update posterior
-            for ij = 1:trainIter
-                b(:,i) =  b(:,i) + phi*Y(ii,i);
-                R(i,:,:) = cholupdate(squeeze(R(i,:,:)),phi);
-                w(:,i) = (1-alpha).*w(:,i) + alpha.*(squeeze(R(i,:,:))\(squeeze(R(i,:,:))'\b(:,i)));
-            end
-        end
-    end
-    
-    % show progress
-    if mod(ii,round((numIter-Z)/10)) == 0
-        disp([num2str(round(ii/numIter*100)),' %'])
-    end
-    iter = iter+1;
-    beta = beta*beta_reduc;
-    beta = max(beta,beta_min);
-end
-toc;
-t_run = toc/(ii-Z);
-disp([char(10),"Elapsed time per iteration: ",num2str(t_run)])
+w    = zeros(2*D,1);
+b    = zeros(2*D,1);
+v    = zeros(2*D,1);
+phi  = zeros(2*D,1);
 
-%% Scale back up
-Y      =      Y.*sig_Y + mu_Y;
-yhatSp = yhatSp.*sig_Y + mu_Y;
-yhat   =   yhat.*sig_Y + mu_Y;
-s2Sp   =   s2Sp.*sig_Y + mu_Y;
-s2     =     s2.*sig_Y + mu_Y;
+RAND  = randn(D,dof);
+R     = eye(2*D,2*D)*sn;
+SIGMA = RAND.*(1./hyp(1,2:end));
+
+% Training Loop
+for ii = 1:length(i_f)
+    s_n   = f(ii,:);
+    ys_n  = yf(ii,:);
+    phi = sf./sqrt(D) .*[cos(SIGMA*s_n')',...
+                         sin(SIGMA*s_n')']';
+    %Update posterior
+    b =  b + phi*ys_n;
+    R = cholupdate(R,phi);
+    w = solve_chol(R,b);
+end
+
+%Prediction loop
+iter = 1;
+for ii = 1:length(i_s)
+    s_n   = s(ii,:);
+    ys_n  = ys(ii,:);
+    phi = sf./sqrt(D) .*[cos(SIGMA*s_n')',...
+                         sin(SIGMA*s_n')']';
+    %Prediction
+    mu_s(iter)   = dot(w,phi);
+    v            = R'\phi;
+    var_s(iter)  = sn2.*(1+dot(v,v));
+    iter = iter + 1;
+end
 
 %% Plot results
-muPred   = yhat(Z:end,:);
-muPredSp = yhatSp(Z:end,:);
-i_plot   = Z:1:ii;
-t        = i_plot*ts;
+clear ha
+resultsISSGP = figure(3); clf(resultsISSGP)
+sphandle(1,1) = subplot(2,1,1);
+hold on
+ha(2) = scatter(f(:,1),yf(:,1),'xb');
+ha(1) = plot(xTrain(1,i_plot),yTrue(1,i_plot),'-k','LineWidth',1.5);
+ha(3) = plot(s(:,1),mu_s,'r','LineWidth',1.5);
+ha(4) = plot(s(:,1),mu_s + 2*sqrt(var_s+sn2),'k');
+        plot(s(:,1),mu_s - 2*sqrt(var_s+sn2),'k');
+legend(ha,'True function','Incr. data','Predicted mean',...
+    'Predictive var.','Interpreter','Latex','FontSize',fSize);
+title('Incremental Sparse Spectrum GP - Gijsberts2013','Interpreter','Latex','FontSize',fSize+8)
+ylabel('y','Interpreter','Latex','FontSize',fSize+4)
+hold off
+clear ha
 
-% Hyperparams
-if param_update == true
-    figure(555),clf(555)
-    fm = 4/5;
-    for i = 1:Q
-        subplot(1,Q,i);
-        %sn_vec(ii) = HYP(ii).hyp.lik;
-        hyp_v = squeeze(HYP(i,:,:));
-        hyp_v(find(hyp_v==0)) = [];
-        hyp_v = reshape(hyp_v,[length(hyp_v)/(n+1),n+1]);
-        nlml(find(nlml==0)) = [];
-        nlml = reshape(nlml,[length(nlml)/(Q),Q]);
-        
-        hold on
-        plot(t(1:trainSteps:end),hyp_v,'LineWidth',1.5);
-        hyp_mean(i,:) = mean(hyp_v(round(length(hyp_v)*fm):end,:));
-        plot(t([1,end]),[hyp_mean(i,:); hyp_mean(i,:)],'--k');
-        hold off
-        xlabel('time (s)');
-        legend('$l_{1}$ $(\theta_1)$','$l_2$ $(\dot{\theta_1})$','$l_3$ $(\ddot{\theta_1})$',...
-            '$l_{1}$ $(\theta_1)$','$l_2$ $(\dot{\theta_1})$','$l_3$ $(\ddot{\theta_1})$',...
-            '$\sigma_s$','Interpreter','Latex');        set(gcf,'Color','w')
-        title('Hyperparameters')
-    end
-    
-    %Store hyperparams in .mat file
-    if saveParams == true
-        sn  = sn;
-        ell = hyp_mean(:,1:end-1);
-        sf  = hyp_mean(:,end);
-        folderName = 'Results\hyperparamSettings\';
-        paramID = [dataID(1:end-4),'_param.mat'];
-        save([folderName,paramID],'sn','ell','sf')
-    end
-end
-%%
-% figure(555),clf(555)
-% hold on
-% plot(t,Y(i_plot,1))
-% plot(t,muPred','LineWidth',2)
-% plot(t,muPred'+sqrt(s2'),'k')
-% plot(t,muPred'-sqrt(s2'),'k')
-% hold off
+%% Continue on extra window
+i_s = [1:700];
+f   = xTrain(:,i_f)';     % Training points
+s   = xTrain(:,i_s)';     % Test points
+a   = b;
+yf  = yTrain(i_f)';
+ys  = yTrain(i_s)';
 
-for i = 1:Q
-    figure(10*i+i),clf(10*i+i);
-    ax71 = subplot(6,1,1);
-    hold on
-    plot(t,X(i_plot,i)','k')
-    plot(t,Xsp(i_plot,i)','r')
-    legend('Measured','Setpoint')
-    ylabel('normalized pos.')
-    title(['Latent function ',num2str(i)])
-    hold off
-    
-    ax72 = subplot(6,1,2);
-    hold on
-    plot(t,X(i_plot,i+n/3)','k')
-    plot(t,Xsp(i_plot,i+n/3)','r')
-    legend('Measured','Setpoint')
-    ylabel('normalized vel.')
-    hold off
-    
-    ax73 = subplot(6,1,3);
-    hold on
-    plot(t,X(i_plot,i+n*2/3)','k')
-    plot(t,Xsp(i_plot,i+n*2/3)','r')
-    legend('Measured','Setpoint')
-    ylabel('normalized acc.')
-    hold off
-    
-    ax74 = subplot(6,1,4:6);
-    hold on
-    plot(t,Y(i_plot,i)','k','LineWidth',1.5);               % Measured signal
-    plot(t,muPred(i_plot-Z+1,i)','--b','LineWidth',1.5);    % Predictions from measurements
-    plot(t,muPredSp(i_plot-Z+1,i)',':r','LineWidth',1.5);   % Predictions from setpoint
-    %Plot variances
-    %plot(t,muPred(i_plot-Z+1,i)'+sqrt(s2(i_plot-Z+1,i)'),'b')
-    %plot(t,muPredSp(i_plot-Z+1,i)'+sqrt(s2Sp(i_plot-Z+1,i)'),'r')
-    %plot(t,muPred(i_plot-Z+1,i)'-sqrt(s2(i_plot-Z+1,i)'),'b')
-    %plot(t,muPredSp(i_plot-Z+1,i)'-sqrt(s2Sp(i_plot-Z+1,i)'),'r')
-    
-    %Plot switch
-    if onlyPredict < numIter
-        plot([onlyPredict onlyPredict]*ts, [min(Y(i_plot-1,i)) max(Y(i_plot-1,i))],':k','LineWidth',1.5)
-    end
-    
-    legend('Measured','\mu_{measure}','\mu_{setpoint}','\sigma_{measure}','\sigma_{setpoint}');
-    xlabel('time (s)')
-    ylabel('Current (mA)')
-    hold off
-    linkaxes([ax71,ax72,ax73,ax74],'x');
-    
-%     figure(150),clf(150)
-%     hold on
-%     plot(t,Y(i_plot)')
-%     plot(t,muPred(i_plot-Z+1)','--k')
-%     plot(t,muPredSp(i_plot-Z+1)',':m','LineWidth',1.5)
-%     legend('Measured','pred. w. Meas.','pred. w. Set.')
-%     xlabel('time (s)')
-%     ylabel('Current (mA)')
-%     hold off
+
+tic
+% Training Loop
+for ii = 1:length(i_loop)
+    s_n   = loop(ii,:);
+    ys_n  = yloop(ii,:);
+    phi = sf./sqrt(D) .*[cos(SIGMA*s_n')',...
+                         sin(SIGMA*s_n')']';
+    %Update posterior
+    b =  b + phi*ys_n;
+    R = cholupdate(R,phi);
+    w = solve_chol(R,b);
 end
 
-%% Error measure
-% Select data
-i_end  = round((t(end)/2)/ts);
-iEnd   = length(muPred);
-iStart = iEnd-i_end;
-shift = -1;
-t_c  = t(iStart:iEnd);
-Y_m  =   Y(iStart+Z+shift:iEnd+Z+shift,:);
-X_m  =   X(iStart+Z+shift:iEnd+Z+shift,1);
-X_sp = Xsp(iStart+Z+shift:iEnd+Z+shift,1);
-%From measured data:
-Y_p  = muPred(iStart:iEnd,:); 
-s2_p = s2(iStart:iEnd,:);
-%From setpoint:
-Y_psp  = muPredSp(iStart:iEnd,:); 
-s2_psp = s2(iStart:iEnd,:);
-
-disp(' ');
-disp(['t+1 predictions (from measurements):']);
-NMSE = errorMeasure(Y_m,Y_p,'method','NMSE','show',true);
-%MAE = errorMeasure(Y_m,Y_p,'method','MAE','show',true);
-%MSE = errorMeasure(Y_m,Y_p,'method','MSE','show',true);
-RMSE = errorMeasure(Y_m,Y_p,'method','RMSE','show',true);
-
-disp(' ');
-disp(['t+1 predictions (from setpoint):']);
-NMSE = errorMeasure(Y_m,Y_psp,'method','NMSE','show',true);
-%MAE = errorMeasure(Y_m,Y_psp,'method','MAE','show',true);
-%MSE = errorMeasure(Y_m,Y_psp,'method','MSE','show',true);
-RMSE = errorMeasure(Y_m,Y_psp,'method','RMSE','show',true);
-
-figure(111),clf(111);
-plot(t_c,Y_m','k')
-hold on
-plot(t_c,Y_p','--b')
-plot(t_c,Y_psp',':r')
-legend('Measured','pred. w. Meas.','pred. w. Set.');
-xlabel('time (s)'); ylabel('Current (mA)')
-title(['1-step ahead predictions    (',num2str(dataID),')'])
-set(gcf,'Color','White')
-hold off
-%}
-% Displacement vs current
-figure(123),clf(123)
-set(gcf,'Color','White')
-sp1 = subplot(2,1,1);
-plot(X_sp,Y_psp)
-xlabel('$\theta_{sp}$','Interpreter','Latex','FontSize',15)
-ylabel('$I_{sp}$ (mA)','Interpreter','Latex','FontSize',12)
-title('Setpoint vs Predicted current')
-legend('\theta_1','\theta_2');
-
-sp2 = subplot(2,1,2);
-plot(X_m,Y_m)
-xlabel('$\theta_{m}$','Interpreter','Latex','FontSize',15)
-ylabel('$I_{m}$ (mA)','Interpreter','Latex','FontSize',12)
-title('Displacement vs Current (measured)')
-linkaxes([sp1,sp2],'xy')
-
-%%
-%{
-figure
-ax1 = subplot(2,1,1);
-hold on
-plot(t,Y(i_plot,i)','k','LineWidth',1.5);               % Measured signal
-plot(t,muPred(i_plot-Z+1,i)','--b','LineWidth',1.5);    % Predictions from measurements
-plot(t,muPredSp(i_plot-Z+1,i)',':r','LineWidth',1.5);   % Predictions from setpoint
-%Plot switch
-if onlyPredict < numIter
-    plot([onlyPredict onlyPredict]*ts, [min(Y(i_plot-1,i)) max(Y(i_plot-1,i))],':k','LineWidth',1.5)
+% Prediction loop 
+iter = 1;
+for ii = 1:length(i_s)
+    s_n   = s(ii,:);
+    ys_n  = ys(ii,:);
+    phi = sf./sqrt(D) .*[cos(SIGMA*s_n')',...
+                         sin(SIGMA*s_n')']';
+    %Prediction
+    mu_s(iter)   = dot(w,phi);
+    v            = R'\phi;
+    var_s(iter)  = sn2.*(1+dot(v,v));
+    iter = iter + 1;
 end
-legend('Measured','\mu_{measure}','\mu_{setpoint}','\sigma_{measure}','\sigma_{setpoint}');
-xlabel('time (s)')
-ylabel('Current (mA)')
-hold off
+timer = toc;
+t_run = toc/(iter);
+fprintf('Number of features:            %i\n',D);
+fprintf('Elapsed time:           total: %.5f ms\n',timer/1e-3)
+fprintf('                per iteration: %f ms \n',t_run/1e-3)
 
-ax2 = subplot(2,1,2);
+%% Plot results
+sphandle(2,1) = subplot(2,1,2);
 hold on
-plot(t(1:trainSteps:end),hyp,'LineWidth',1.5);
-hyp_mean(i,:) = mean(hyp(round(length(hyp)*fm):end,:));
-plot(t([1,end]),[hyp_mean(i,:); hyp_mean(i,:)],'--k');
+ha(2) = scatter(f(:,1),yf(:,1),'xk');
+ha(2).MarkerFaceAlpha = .6;
+ha(2).MarkerEdgeAlpha = .6;
+ha(3) = scatter(loop(:,1),yloop(:,1),'xb');
+ha(1) = plot(xTrain(1,i_plot),yTrue(1,i_plot),'-k','LineWidth',1.5);
+ha(4) = plot(s(:,1),mu_s,'r','LineWidth',1.5);
+ha(5) = plot(s(:,1),mu_s + 2*sqrt(var_s+sn2),'k');
+        plot(s(:,1),mu_s - 2*sqrt(var_s+sn2),'k');
+legend(ha,'True function','Incr. data','Incr. data','Predicted mean',...
+    'Predictive var.','Interpreter','Latex','FontSize',fSize);
+ylabel('y','Interpreter','Latex','FontSize',fSize+4)
 hold off
-xlabel('time (s)');
-legend('$l_1$ $(\theta)$','$l_2$ $(\dot{\theta})$','$l_3$ $(\ddot{\theta})$','$\sigma_s$','Interpreter','Latex');
-set(gcf,'Color','w')
-hold off
-linkaxes([ax1,ax2],'x');
-%}
+clear ha
 
-%{
+[resultsISSGP,sphandle] = subplots(resultsISSGP,sphandle);
 
-figure(8),clf(8);
+%% Error
+error = rms(mu_s - yTrain(1,i_s)');
+fprintf('RMS error:  complete interval: %f \n',error)
 
-ax81 = subplot(3,1,1);
-hold on
-plot(t,X(i_plot,3)')
-plot(t,Xsp(i_plot,3)','m')
-legend('Measured','Setpoint')
-ylabel('normalized acc.')
-xlim([10.4 10.8])
+error = rms(mu_s(i_total) - yTrain(1,i_total)');
+fprintf('            measured interval: %f \n\n',error)
 
-hold off
-
-ax82 = subplot(3,1,2);
-hold on
-plot(t,Y(i_plot)')
-plot(t,muPred','--k')
-plot(t,muPredSp',':m','LineWidth',1.5)
-legend('Measured','pred. w. Meas.','pred. w. Set.')
-ylabel('Current (mA)')
-xlim([10.4 10.8])
-hold off
-
-mu_muPredSp  = mean(muPredSp);
-sig_muPredSp = std(muPredSp);
-muPredSp = (muPredSp - mu_muPredSp) ./ sig_muPredSp;
-
-ax83 = subplot(3,1,3);
-hold on
-plot(t,muPredSp')
-plot(t,muPredSp' - Xsp(i_plot,3)'*(1))
-%plot(t,muPredSp'-Xsp(i_plot,2)'*(1002/2.4521))
-xlim([10.4 10.8])
-hold off
-%}
+% For SSGP.m
+xTrain  = xTrain;
+yTrain  = yTrain';
+yTrue   = yTrue';
---
+++ SSGP.m
@@ -0,0 +1,270 @@
+
+%Run incremental FITC script
+I_FITC
+%Run incremental Sparse Spectrum GP script
+I_SSGP
+
+fig2pdf = 0;
+pdf2ipe = 0;
+
+alpha  = 0.5;   % VFE,  alpha = 0
+                % SPEP, 0 < alpha < 1
+                % FITC, alpha = 1
+
+fprintf('  ----  Streaming sparse GP - Bui2015  ----  \n')
+%% (Offline) Streaming Sparse Gaussian Process (initial step)
+i_f = 1:500;
+i_s = [1:10:500];
+i_b = [1:10:500];
+
+%a  = xTrain(i_a)';      % Old inducing points
+b  = xTrain(:,i_b)';     % New inducing points
+f  = xTrain(:,i_f)';     % Training points
+s  = xTrain(:,i_s)';     % Test points
+
+%ya = yTrain(i_b)';
+yb = yTrain(i_b)';    
+yf = yTrain(i_f)';
+ys = yTrain(i_s)';
+err = yf;
+
+tic
+%Ma = length(a);
+Mb = length(b);
+Mf = length(f);
+Ms = length(s);
+
+Kff     = SEcov(f,f,hyp);
+Kfdiag  = diag(Kff);
+Kbf     = SEcov(b,f,hyp);
+Kbb     = SEcov(b,b,hyp) + eye(Mb)*jitter;
+Kbs     = SEcov(b,s,hyp);
+
+Lb          = chol(Kbb,'lower');
+Lbinv_Kbf   = solve_lowerTriangular(Lb,Kbf);
+
+Lbinv_Kbs   = solve_lowerTriangular(Lb, Kbs);
+
+Qfdiag      = Kfdiag - diag(Lbinv_Kbf'*Lbinv_Kbf);
+%Qfdiag      = Kfdiag' - sum(Lbinv_Kbf.^2);
+Dff         = sn + alpha.*Qfdiag;
+Lbinv_Kbf_LDff = Lbinv_Kbf./sqrt(Dff');
+
+D = eye(Mb) + Lbinv_Kbf_LDff*Lbinv_Kbf_LDff';
+LD = chol(D,'lower');
+
+LDinv_Lbinv_Kbs = solve_lowerTriangular(LD, Lbinv_Kbs);
+LDinv_Lbinv_Kbf = solve_lowerTriangular(LD,Lbinv_Kbf);
+
+Sinv_y          = (yf./Dff')';
+c               = Lbinv_Kbf*Sinv_y;
+LDinv_Lbinv_Kbf_c  = LDinv_Lbinv_Kbf*Sinv_y;
+LDinv_c         = solve_lowerTriangular(LD,c);
+
+
+%% Approximate log marginal Likelihood
+%{
+bound = 0;
+% constant term
+bound = - 0.5*Mf*log(2*pi);
+% quadratic term
+bound = bound - 0.5 * sum(err.^2/ Dff');
+bound = bound + 0.5 * sum(LDinv_c.^2);
+
+% log det term
+bound = bound - 0.5 * sum(log(Dff));
+bound = bound - sum(log(diag(LD)));
+
+% trace-like term
+bound = bound - 0.5*(1-alpha)/alpha * sum(log(Dff./sn));
+%}
+%% Prediction 
+%Predictive mean
+%m = LDinv_Lbinv_Kbs'*LDinv_Lbinv_Kbf_c;
+m = LDinv_Lbinv_Kbs'*LDinv_c;
+
+%Prediction variance (matrix)
+Kss  = SEcov(s,s,hyp) + eye(Ms)*jitter;
+var1 = Kss;
+var2 = -Lbinv_Kbs'*Lbinv_Kbs;
+var3 = LDinv_Lbinv_Kbs'*LDinv_Lbinv_Kbs;
+Su   = var1 + var2 + var3;
+
+%Prediction variance (vector)
+var  = diag(Su);
+toc
+%% Plot result 
+resultsSSGP = figure(2); clf(2);
+sphandle(1,1) = subplot(2,1,1);
+hold on
+ha(2) = scatter(f(:,1),yf,'xb');
+ha(2).MarkerFaceAlpha = .6;
+ha(2).MarkerEdgeAlpha = .6;
+ha(3) = scatter(b(:,1),zeros(size(b,1),1),ones(size(b,1),1)*10,'ok','filled');
+ha(1) = plot(xTrain(1,i_plot),yTrue(i_plot,1),'k','LineWidth',1.5);
+ha(4) = plot(s(:,1),m,'-r','LineWidth',2);
+ha(5) = plot(s(:,1),m + 2*sqrt(var+sn2),'-k');
+        plot(s(:,1),m - 2*sqrt(var+sn2),'-k');
+ylabel('y','Interpreter','Latex','FontSize',fSize+4);
+title('Streaming Sparse GP - Bui2017','Interpreter','Latex','FontSize',fSize+8);
+legend(ha,'True function','Data in window','Inducing points','Predicted mean',...
+    'Predictive var.','Interpreter','Latex','FontSize',fSize);
+hold off
+
+
+sphandle(2,1) = subplot(2,1,2);
+hold on
+han(3) = scatter(f(:,1),yf,'xk');
+han(3).MarkerFaceAlpha = .5; 
+han(3).MarkerEdgeAlpha = .5;
+hold off
+
+%% Online Streaming Sparse Gaussian Process
+i_f = 600:700;
+i_s = [1:700];
+f   = xTrain(:,i_f)';     % Training points
+s   = xTrain(:,i_s)';     % Test points
+a   = b;
+yf  = yTrain(i_f)';
+ys  = yTrain(i_s)';
+
+tic;
+M       = length(a);
+M_old   = floor(0.7*M);
+M_new   = M - M_old;
+old_Z   = b(randi(M,1,M_old),:);
+new_Z   = f(randi(size(f,1),1,M_new),:);
+b       = [old_Z;new_Z];
+b       = u_new; % From I_FITC
+
+Ma = length(a);
+Mb = length(b);
+Mf = length(f);
+Ms = length(s);
+
+Saa = Su;
+ma  = m;
+Kaa_old = Kbb;
+Kfdiag  = diag(SEcov(f,f,hyp));
+Kbf     = SEcov(b,f,hyp);
+Kbb     = SEcov(b,b,hyp) + eye(Ma)*jitter;
+Kba     = SEcov(b,a,hyp);
+Kaa_cur = SEcov(a,a,hyp) + eye(Ma)*jitter;
+Kaa     = Kaa_old + eye(Ma)*jitter;
+
+Lb          = chol(Kbb,'lower');
+Lbinv_Kbf   = solve_lowerTriangular(Lb,Kbf);
+
+Qfdiag      = Kfdiag - diag(Lbinv_Kbf'*Lbinv_Kbf);
+%Qfdiag      = Kfdiag' - sum(Lbinv_Kbf.^2);
+Dff         = sn + alpha.*Qfdiag;
+Lbinv_Kbf_LDff = Lbinv_Kbf./sqrt(Dff');
+d1 = Lbinv_Kbf_LDff*Lbinv_Kbf_LDff';
+
+Lbinv_Kba = solve_lowerTriangular(Lb, Kba); 
+Kab_Lbinv = Lbinv_Kba';
+
+Sainv_Kab_Lbinv = Saa\Kab_Lbinv;
+Kainv_Kab_Lbinv = Kaa\Kab_Lbinv;
+Da_Kab_Lbinv = Sainv_Kab_Lbinv - Kainv_Kab_Lbinv;
+d2 = Lbinv_Kba*Da_Kab_Lbinv;
+
+Kaadiff = Kaa_cur - Kab_Lbinv*Lbinv_Kba;
+LM = chol(Kaadiff,'lower');
+LMT = LM';
+Sainv_LM = Saa\LM;
+Kainv_LM = Kaa\LM;
+SK_LM = Sainv_LM - Kainv_LM;
+LMT_SK_LM = LMT*SK_LM;
+Q = eye(Ma) + alpha*LMT_SK_LM;
+LQ = chol(Q,'lower');
+
+LMT_Da_Kab_Lbinv = LMT*Da_Kab_Lbinv;
+Qinv_t1 = Q\LMT_Da_Kab_Lbinv;
+t1_Qinv_t1 = LMT_Da_Kab_Lbinv'*Qinv_t1;
+d3 = -alpha * t1_Qinv_t1;
+
+D = eye(Mb) + d1 + d2 + d3;
+D = D + eye(Mb)* jitter;
+LD = chol(D,'lower');
+
+Sainv_ma            = Saa\ma;
+LMT_Sainv_ma        = LMT*Sainv_ma;
+Lbinv_Kba_Da        = Da_Kab_Lbinv';
+Lbinv_Kba_Da_LM     = Lbinv_Kba_Da*LM;
+Qinv_LMT_Sainv_ma   = Q\LMT_Sainv_ma;
+Sinv_y              = (yf./Dff')';
+c1      = Lbinv_Kbf*Sinv_y;
+c2      = Lbinv_Kba*Sainv_ma;
+c3      = - alpha*Lbinv_Kba_Da_LM*Qinv_LMT_Sainv_ma;
+c       = c1 + c2 + c3;
+
+LDinv_c = solve_lowerTriangular(LD, c);
+LSa     = chol(Saa,'lower');
+La      = chol(Kaa,'lower');
+
+%Predict
+Kbs = SEcov(b,s,hyp);
+Lbinv_Kbs = solve_lowerTriangular(Lb, Kbs);
+LDinv_Lbinv_Kbs = solve_lowerTriangular(LD, Lbinv_Kbs);
+
+%Predictive mean
+m = LDinv_Lbinv_Kbs'*LDinv_c;
+
+%Prediction variance (matrix)
+Kss = SEcov(s,s,hyp) + eye(Ms)*jitter;
+var1 = Kss;
+var2 = -Lbinv_Kbs'*Lbinv_Kbs;
+var3 = LDinv_Lbinv_Kbs'*LDinv_Lbinv_Kbs;
+Su  = var1 + var2 + var3;
+
+%Prediction variance (vector)
+var = diag(Su);
+
+
+timer = toc;
+fprintf('Number of inducing points:     %i\n',Mu);
+fprintf('Elapsed time:           total: %.5f ms\n',timer/1e-3)
+
+%% Plot results
+resultsSSGP;
+sphandle(2,1);
+hold on
+han(2) = plot(f(:,1),yf,'xb');
+han(4) = scatter(b(:,1),zeros(size(b,1),1),ones(size(b,1),1)*10,'ok','filled');
+han(1) = plot(xTrain(1,i_plot),yTrue(i_plot,1),'k','LineWidth',1.5);
+han(5) = plot(s(:,1),m,'-r','LineWidth',2);
+han(6) = plot(s(:,1),m + 2*sqrt(var + sn2),'-k');
+         plot(s(:,1),m - 2*sqrt(var + sn2),'-k');
+legend(han,'True function','Data in window','Data outside window',...
+    'Inducing points','Predicted mean','Predictive var.','Interpreter','Latex','FontSize',fSize);
+%xlim([0,4]);
+xlabel('t (s)','Interpreter','Latex','FontSize',fSize+4);
+ylabel('y','Interpreter','Latex','FontSize',fSize+4);
+hold off
+
+[resultsSSGP,sphandle] = subplots(resultsSSGP,sphandle);
+
+%% Error
+error = rms(m - yTrain(i_s,1));
+fprintf('RMS error:  complete interval: %f \n',error)
+
+error = rms(m(i_total) - yTrain(i_total,1));
+fprintf('            measured interval: %f \n\n',error)
+
+%% Save Figures
+if(fig2pdf)
+    saveas(resultsIFITC,fullfile(pwd,'Images','IFITC.pdf'));
+    saveas(resultsISSGP,fullfile(pwd,'Images','ISSGP.pdf'));
+    saveas(resultsSSGP,fullfile(pwd,'Images','SSGP.pdf'));
+end
+
+%% PDF2IPE
+if(pdf2ipe)
+    pdf2ipepdf_v2(fullfile(pwd,'Images'),{''},{''});
+end
+
+%%
+xTrain  = xTrain;
+yTrain  = yTrain';
+yTrue   = yTrue';
